0f07e24595b4df8c9d9f2072764abbf0fd261fc6
Aleksei <lexxbar@mail.ru>
2022-07-19 14:16:41 +0200

    Day

---
+++ DZ_03/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter number; ");
+int x = Convert.ToInt32 (Console.ReadLine());
+
+if ( x < 6 ) 
+    {
+        Console.WriteLine("WorkDay");
+    }   
+if ( x == 6 )
+    {
+        Console.WriteLine("WeekEnd");
+    }
+if ( x == 7 )
+    {
+        Console.WriteLine("WeekEnd");
+    }    
+if ( x > 7 )
+    {
+        Console.WriteLine("Error");
+    }
+
+
+
